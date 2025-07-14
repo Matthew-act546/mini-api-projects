587472776f0c8aa72b2997abb3a3064c52259d34
matthew <matthewyt765@gmail.com>
2025-07-15 06:53:37 +0800

    change into async function

--- bible-random-verse/bibleAPI.js
+++ bible-random-verse/bibleAPI.js
@@ -1,10 +1,10 @@
 const verseContainer = document.getElementById("verse-container");
 
-function getVerse() {
-  fetch("https://bible-api.com/data/web/random")
-  .then(response => response.json())
-  .then(data => {
-    const verse = data.random_verse;
+async function getVerse() {
+  try {
+    const response = await fetch("https://bible-api.com/data/web/random");
+    const verseJson = await response.json();
+    const verse = verseJson.random_verse;
 
     const verseText = document.createElement("p");
     verseText.textContent = `${verse.text.trim()}`;
@@ -12,13 +12,13 @@ function getVerse() {
     const reference = document.createElement("h5");
     reference.textContent = `${verse.book} ${verse.chapter}:${verse.verse}`;
 
-    verseContainer.innerHTML = ""; 
+    verseContainer.innerHTML = "";
     verseContainer.appendChild(reference);
     verseContainer.appendChild(verseText);
-  })
-  .catch(err => {
-    verseContainer.textContent = "Failed to load verse ";
+
+    console.log(verseJson);
+  } catch (err) {
+    verseContainer.textContent = "Failed to load verse 😢";
     console.error("Error:", err);
-  });
-  
+  }
 }
